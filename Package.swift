45ce356d2ea379a86ee8996d4379fade6ddde5c8
Alexander Volkov <volk@frgroup.ru>
2020-05-13 11:34:58 +0300

    Version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TCNClient",
     platforms: [
-      .iOS(.v10),
+      .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/christophhagen/CCurve25519.git", from: "1.0.2")
+        .package(url: "https://github.com/christophhagen/CCurve25519.git", from: "1.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
